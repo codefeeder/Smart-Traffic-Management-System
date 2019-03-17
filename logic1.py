54e50d70869b5f09af90e08a043262756a9153a5
codefeeder <singhalsiddharth12@gmail.com>
2019-03-17 22:52:46 +0530

    driver program

---
+++ logic1.py
@@ -0,0 +1,193 @@
+import datetime
+import random
+import numpy as np
+import io
+import pickle
+arr=[]
+tr = 0
+arr.append(tr) #0
+time_at_recieved = datetime.datetime.now()
+arr.append(time_at_recieved)#1
+severity_index = 0
+arr.append(severity_index)#2
+tr_1 = 0
+arr.append(tr_1)#3
+time_at_recieved_1 = datetime.datetime.now()
+arr.append(time_at_recieved_1)#4
+severity_index_1 = 0
+arr.append(severity_index_1)#5
+crossing = 1
+arr.append(crossing)#6
+signal = random.randrange(0, 4)
+arr.append(signal)#7
+arr=np.array(arr)
+compressed_array = io.BytesIO()
+# np.savez_compressed(compressed_array,arr)
+with open('filename.pickle', 'wb') as handle:
+    pickle.dump(arr, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def time_update(new_time, severity):
+    global tr
+    global tr_1
+    global time_at_recieved
+    global severity_index
+    global time_at_recieved_1
+    global severity_index_1
+    global crossing
+    global signal
+    # arr = np.load(compressed_array)['arr_0']
+    with open('filename.pickle', 'rb') as handle:
+        arr = pickle.load(handle)
+    arr = arr.tolist()
+    tr_1 = new_time
+    arr[3] = tr_1
+    time_at_recieved_1 = datetime.datetime.now()
+    arr[4] = time_at_recieved_1
+    severity_index_1 = severity
+    arr[5] = severity_index_1
+    arr=np.array(arr)
+    # np.savez_compressed(compressed_array,arr)
+    with open('filename.pickle', 'wb') as handle:
+        pickle.dump(arr, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def conclusion(density):
+    global tr
+    global tr_1
+    global time_at_recieved
+    global severity_index
+    global time_at_recieved_1
+    global severity_index_1
+    global crossing
+    global signal
+    # arr = np.load(compressed_array)['arr_0']
+    with open('filename.pickle', 'rb') as handle:
+        arr = pickle.load(handle)
+    arr = arr.tolist()
+# tr = 0
+# arr.append(tr) #0
+# time_at_recieved = datetime.datetime.now()
+# arr.append(time_at_recieved)#1
+# severity_index = 0
+# arr.append(severity_index)#2
+# tr_1 = 0
+# arr.append(tr_1)#3
+# time_at_recieved_1 = datetime.datetime.now()
+# arr.append(time_at_recieved_1)#4
+# severity_index_1 = 0
+# arr.append(severity_index_1)#5
+# crossing = 1
+# arr.append(crossing)#6
+# signal = random.randrange(0, 4)
+# arr.append(signal)#7
+    tr = arr[0]
+    time_at_recieved = arr[1]
+    severity_index = arr[2]
+    tr_1 = arr[3]
+    time_at_recieved_1 = arr[4]
+    severity_index_1 = arr[5]
+    crossing = arr[6]
+    signal = arr[7]
+
+    if tr == 0:
+        if tr_1 != 0:
+            tr = tr_1
+            time_at_recieved = time_at_recieved_1
+            severity_index = severity_index_1
+            tr_1 = 0
+            time_at_recieved_1 = 0
+            severity_index_1 = 0
+            # arr = np.load(compressed_array)['arr_0']
+            with open('filename.pickle', 'rb') as handle:
+                arr = pickle.load(handle)
+            arr = arr.tolist()
+            arr[0] = tr
+            arr[1] = time_at_recieved
+            arr[2] = severity_index
+            arr[3] = tr_1
+            arr[4] = time_at_recieved_1
+            arr[5] = severity_index_1
+            arr=np.array(arr)
+            # np.savez_compressed(compressed_array,arr)
+            with open('filename.pickle', 'wb') as handle:
+                pickle.dump(arr, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        crossing_str = str(crossing)
+        #print ('hi')
+        ret = [crossing_str, '', 'R', 'R', 'R', 'R']
+        i = 0
+        max = -1
+        for k in range(4):
+            if density[k] > max:
+                max = density[k]
+                i = k
+        ret[i + 2] = 'G'
+        return ret
+    else:
+        time_now = datetime.datetime.now()
+        elapsed_time = time_now - time_at_recieved
+        diff = divmod(elapsed_time.total_seconds(), 60)
+        diff_sec = diff[0] * 60 + diff[1]
+        in_time = tr - diff_sec
+        if (tr - diff_sec) < 5:
+            signal_now = signal
+            if (tr <= diff_sec):
+                crossing = crossing + 1
+                tr = 0
+                time_at_recieved = 0
+                severity_index = 0
+                signal = random.randrange(0, 4)
+                # arr = np.load(compressed_array)['arr_0']
+                with open('filename.pickle', 'rb') as handle:
+                    arr = pickle.load(handle)
+                arr = arr.tolist()
+                arr[6] = crossing
+                arr[0] = tr
+                arr[2] = severity_index
+                arr[1] = time_at_recieved
+                arr[7] = signal
+                arr=np.array(arr)
+                # np.savez_compressed(compressed_array,arr)
+                with open('filename.pickle', 'wb') as handle:
+                    pickle.dump(arr, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            crossing_str = str(crossing)
+            if (in_time<0):
+                in_time = 0
+            in_time_str = str(in_time)
+            ret = [crossing_str, in_time_str, 'R', 'R', 'R', 'R']
+            ret[signal_now + 2] = 'G'
+            return ret
+        else:
+            time_now = datetime.datetime.now()
+            elapsed_time = time_now - time_at_recieved
+            diff = divmod(elapsed_time.total_seconds(), 60)
+            diff_sec = diff[0] * 60 + diff[1]
+            tre_1 = density[signal] + severity_index * 10 * (1 / (diff_sec))
+            i = 0
+            tr_2 = -1
+            for k in range(4):
+                if density[k] > tr_2:
+                    tr_2 = density[k]
+                    i = k
+            if tr_2 > tre_1:
+                crossing_str = str(crossing)
+                if (in_time<0):
+                    in_time = 0
+                in_time_str = str(in_time)
+                ret = [crossing_str, in_time_str, 'R', 'R', 'R', 'R']
+                ret[i + 2] = 'G'
+            else:
+                crossing_str = str(crossing)
+                if (in_time<0):
+                    in_time = 0            
+                in_time_str = str(in_time)
+                ret = [crossing_str, in_time_str, 'R', 'R', 'R', 'R']
+                ret[signal + 2] = 'G'
+            return ret
+
+
+# initialize()
+
+# print(conclusion([10, 20, 30, 4]))
+# time_update(80,0)
+# print (tr_1)
